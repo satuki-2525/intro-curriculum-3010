45a1be4a724dd5455f94dba9a7d683d67fcc2fc5
Satuki Yamabuki <satuki.book.2525@gmail.com>
2022-10-01 06:19:47 +0900

    修正

--- test.js
+++ test.js
@@ -1,6 +1,11 @@
 'use strict';
-const todo = require('./index.js');
 const assert = require('assert');
+
+const fs = require('fs');
+fs.unlinkSync('./tasks.json');
+const todo = require('./index.js');
+
+
 
 // add と list のテスト
 todo.add('ノートを買う');
